19a48efb6bafa0b903541f0422e886f5ca62ff91
JuliaZatonatska <zatonatskajulia@knu.ua>
2025-02-11 10:07:15 +0200

    найбільше спільний дільник

--- utils.py
+++ utils.py
@@ -2,3 +2,10 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
+
+def nsd(a, b):
+    while b != 0:
+        remainder = a - b * (a // b)  #remainder це з анг залишок
+        a = b
+        b = remainder
+    return a
