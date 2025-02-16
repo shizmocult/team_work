445dfddc0f54f0426d0e780aa0dcb4bd0dc8d3fa
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-02-16 19:36:37 +0200

    Added call to is_prime function in main.py

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from utils import factorial, nsd
 print(f"Factorial of 5: {factorial(5)}")
 
 print("Найбільший спільний дільник:", nsd(x, y))
+
+from utils import is_prime
+
+number = 11
+if is_prime(number):
+    print(f"{number} is prime")
+else:
+    print(f"{number} is not prime")
+
